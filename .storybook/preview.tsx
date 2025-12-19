93cbc1cf3476ad53f37df1426ab5538fa274924c
LouisAugustine <nzlouis.com@gmail.com>
2025-12-19 18:22:45 +1300

    feat: Add Storybook setup, configure Next.js image domain for Unsplash, and update gitignore.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
-import "../styles/globals.css";
+import "../src/app/globals.css";
+import * as NextImage from "next/image";
+
+// Mock next/image
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props: any) => <OriginalNextImage {...props} unoptimized />,
+});
 
 const preview: Preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: {
+        mobile: { name: "Mobile", styles: { width: "375px", height: "667px" } },
+        tablet: {
+          name: "Tablet",
+          styles: { width: "768px", height: "1024px" },
+        },
+        desktop: {
+          name: "Desktop",
+          styles: { width: "1024px", height: "768px" },
+        },
+        large: { name: "Large", styles: { width: "1440px", height: "900px" } },
+      },
+    },
+  },
   decorators: [
     (Story) => (
       <div style={{ padding: 20 }}>
--- .storybook/vite.config.ts
+++ .storybook/vite.config.ts
@@ -4,10 +4,11 @@ import path from "path";
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "../"),
-      "@/lib": path.resolve(__dirname, "../lib"),
-      "@/hooks": path.resolve(__dirname, "../hooks"),
-      "@/components": path.resolve(__dirname, "../components"),
+      "@": path.resolve(__dirname, "../src"),
+      "@/lib": path.resolve(__dirname, "../src/lib"),
+      "@/hooks": path.resolve(__dirname, "../src/hooks"),
+      "@/components": path.resolve(__dirname, "../src/components"),
+      "@/app": path.resolve(__dirname, "../src/app"),
     },
   },
 });
